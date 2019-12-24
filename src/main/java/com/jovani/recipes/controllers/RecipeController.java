f1d7cb67bbc233b354b3d77f548a340c0b1c7269
Jovani Oliveira Brasil <jovanibrasil@gmail.com>
2019-12-24 16:20:05 -0300

    Fix defficulty dropdown and add recipe create button

--- src/main/java/com/jovani/recipes/controllers/RecipeController.java
+++ src/main/java/com/jovani/recipes/controllers/RecipeController.java
@@ -5,10 +5,7 @@ import com.jovani.recipes.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @Controller
@@ -20,28 +17,28 @@ public class RecipeController {
         this.recipeService = recipeService;
     }
 
-    @RequestMapping("/recipe/{id}")
+    @GetMapping("/recipe/{id}")
     public String showRecipeById(@PathVariable String id, Model model){
         log.debug("Getting show recipe page");
         model.addAttribute("recipe", this.recipeService.findById(Long.valueOf(id)));
         return "recipe/show";
     }
 
-    @RequestMapping("/recipe/{id}/update")
+    @GetMapping("/recipe/{id}/update")
     public String editRecipeById(@PathVariable String id, Model model){
         RecipeCommand recipeCommand = this.recipeService.findRecipeCommand(Long.valueOf(id));
         model.addAttribute("recipe", recipeCommand);
         return "recipe/recipeform";
     }
 
-    @RequestMapping("/recipe/{id}/delete")
+    @GetMapping("/recipe/{id}/delete")
     public String deleteRecipeById(@PathVariable String id){
         log.debug("Deleting post with id = {}", id);
         this.recipeService.deleteById(Long.valueOf(id));
         return "redirect:/";
     }
 
-    @RequestMapping("/recipe/new")
+    @GetMapping("/recipe/new")
     public String createRecipe(Model model){
         model.addAttribute("recipe", new RecipeCommand());
         return "recipe/recipeform";
